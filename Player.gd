1a9cbefe0589189e32c4fbc8b89853fc6600015a
Matteo Calderaro <matteo.calderaro0@gmail.com>
2021-10-20 19:01:36 +0200

    Premier jeu (ça marche presque)

---
+++ Player.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+signal hit
+
+export var speed = 400  # vitesse du joueur (pixel/s)
+var screen_size  # taille de la fenetre du jeu
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+# colision
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disable", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
